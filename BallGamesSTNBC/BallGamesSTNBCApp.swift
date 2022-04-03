2b2b08a3bd5add38975c996dd7dd0ebfd76c53c1
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-03 14:29:09 +0300

    Initial Commit

---
+++ BallGamesSTNBC/BallGamesSTNBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallGamesSTNBCApp.swift
+//  BallGamesSTNBC
+//
+//  Created by Vahtee Boo on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BallGamesSTNBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
